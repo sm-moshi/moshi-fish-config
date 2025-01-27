b3a4e760b5f8d28eaa48f4a59656c9e5ee474240
sm-moshi <stuartmeya@proton.me>
2025-01-27 02:24:17 +0100

    Just variable stuff

--- conf.d/programming.fish
+++ conf.d/programming.fish
@@ -1,36 +1,46 @@
 # Programming tools configuration
 
+# Homebrew paths
+set -gx BREW_PREFIX /opt/homebrew
+set -gx BREW_INCLUDE "$BREW_PREFIX/include" \
+    "$BREW_PREFIX/opt/openssl@3/include" \
+    "$BREW_PREFIX/opt/ncurses/include"
+"$BREW_PREFIX/opt/curl/include"
+
+set -gx BREW_LIB "$BREW_PREFIX/lib" \
+    "$BREW_PREFIX/opt/openssl@3/lib" \
+    "$BREW_PREFIX/opt/ncurses/lib -ltinfo" \
+    "$BREW_PREFIX/opt/curl/lib"
+
 # OpenSSL configuration
-set -gx OPENSSL_PATH "/opt/homebrew/opt/openssl@3"
-set -gx LDFLAGS "-L$OPENSSL_PATH/lib"
-set -gx CPPFLAGS "-I$OPENSSL_PATH/include"
-set -gx PKG_CONFIG_PATH "$OPENSSL_PATH/lib/pkgconfig"
+set -gx OPENSSL_PATH "$BREW_PREFIX/opt/openssl@3"
 
 # PKG_CONFIG configuration
 set -gx PKG_CONFIG_PATH \
-    "/opt/homebrew/opt/openssl@3/lib/pkgconfig" \
-    /opt/homebrew/opt/llvm/lib/pkgconfig \
-    /opt/homebrew/opt/curl/lib/pkgconfig \
-    /opt/homebrew/opt/ncurses/lib/pkgconfig \
-    /opt/homebrew/opt/libffi/lib/pkgconfig \
-    "/opt/homebrew/opt/tcl-tk@8/lib/pkgconfig" \
-    /opt/homebrew/lib/pkgconfig
+    "$OPENSSL_PATH/lib/pkgconfig" \
+    "$BREW_PREFIX/opt/llvm/lib/pkgconfig" \
+    "$BREW_PREFIX/opt/curl/lib/pkgconfig" \
+    "$BREW_PREFIX/opt/ncurses/lib/pkgconfig" \
+    "$BREW_PREFIX/opt/libffi/lib/pkgconfig" \
+    "$BREW_PREFIX/opt/tcl-tk@8/lib/pkgconfig" \
+    "$BREW_PREFIX/lib/pkgconfig"
 
 # LLVM configuration (optimized for Apple Silicon)
-set -gx CC /opt/homebrew/opt/llvm/bin/clang
-set -gx CXX "/opt/homebrew/opt/llvm/bin/clang++"
-set -gx LD "/opt/homebrew/opt/llvm/bin/ld.lld"
-set -gx AR /opt/homebrew/opt/llvm/bin/llvm-ar
-set -gx NM /opt/homebrew/opt/llvm/bin/llvm-nm
-set -gx RANLIB /opt/homebrew/opt/llvm/bin/llvm-ranlib
+set -gx CC "$BREW_PREFIX/opt/llvm/bin/clang"
+set -gx CXX "$BREW_PREFIX/opt/llvm/bin/clang++"
+set -gx LD "$BREW_PREFIX/opt/llvm/bin/ld.lld"
+set -gx AR "$BREW_PREFIX/opt/llvm/bin/llvm-ar"
+set -gx NM "$BREW_PREFIX/opt/llvm/bin/llvm-nm"
+set -gx RANLIB "$BREW_PREFIX/opt/llvm/bin/llvm-ranlib"
 
 # Compiler flags
-set -gx CFLAGS "-O3 -flto=thin -march=native -pipe -I/opt/homebrew/opt/llvm/include"
-set -gx CXXFLAGS "-O3 -flto=thin -march=native -pipe -I/opt/homebrew/opt/llvm/include"
-set -gx LDFLAGS "$LDFLAGS -L/opt/homebrew/opt/llvm/lib -L/opt/homebrew/opt/llvm/lib/c++ -L/opt/homebrew/opt/llvm/lib/unwind -lunwind -Wl,-z,relro -Wl,-z,now -fuse-ld=lld"
+set -gx CFLAGS "-O3 -flto=thin -march=native -pipe -I$BREW_PREFIX/opt/llvm/include"
+set -gx CXXFLAGS "$CFLAGS"
+set -gx LDFLAGS "-L$BREW_LIB -L$BREW_PREFIX/opt/llvm/lib -L$BREW_PREFIX/opt/llvm/lib/c++ -L$BREW_PREFIX/opt/llvm/lib/unwind -lunwind -Wl,-z,relro -Wl,-z,now -fuse-ld=lld"
+set -gx CPPFLAGS "-I$BREW_INCLUDE"
 
 # Rust configuration
-set -gx RUSTFLAGS "-C link-arg=-fuse-ld=lld -C target-cpu=native -C opt-level=3 -C embed-bitcode=yes -C debuginfo=2"
+set -gx RUSTFLAGS "-C link-arg=-fuse-ld=lld -C target-cpu=native -C opt-level=3 -C embed-bitcode=yes"
 
 # Java configuration (ASDF-managed)
 if type -q asdf
